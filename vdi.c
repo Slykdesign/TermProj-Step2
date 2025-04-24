4db94b6d855cc05c37c6ac86524c1b01ff9347a6
Hayden Slyk <132722612+Slykdesign@users.noreply.github.com>
2025-04-24 01:09:01 +0000

    Add files via upload

--- vdi.c
+++ vdi.c
@@ -179,17 +179,14 @@ void displayVDIHeader(VDIFile *vdi) {
 
 void displayVDITranslationMap(VDIFile *vdi) {
     uint8_t mbr[512];
-    lseek(vdi->fd, 0, SEEK_SET);
-    read(vdi->fd, mbr, 512);
+    vdiSeek(vdi, 0, SEEK_SET);
+    vdiRead(vdi, mbr, 512);
     displayBuffer(mbr, 512, 0x0);
 }
 
 void displayMBR(VDIFile *vdi) {
-    uint8_t mbr[512];
-    // Seek to logical offset 0 in the virtual disk
+    uint8_t mbr[256];
     vdiSeek(vdi, 0, SEEK_SET);
-    // Read 512 bytes from the virtual disk
     vdiRead(vdi, mbr, sizeof(mbr));
-    // Display the contents of the MBR buffer
     displayBuffer(mbr, sizeof(mbr), 0);
 }
